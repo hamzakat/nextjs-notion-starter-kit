6dfc42045c9ad1e0d56eb70dc33f8681d551c25b
Hamza <superusermhk@gmail.com>
2023-04-23 09:04:30 +0300

    Add prefix URL to images

--- next.config.js
+++ next.config.js
@@ -17,7 +17,8 @@ module.exports = withBundleAnalyzer({
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    path: `https://${process.env.VERCEL_URL}`
   },
   assetPrefix: `https://${process.env.VERCEL_URL}` // it is necessary to load the assets in case we do a rewrite() request from another next.js app
 })
